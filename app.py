708e795dd9928fa226787b1eed386b9222426a7c
jakobkg <j@kob.codes>
2020-09-03 11:36:27 +0200

    Beginnings of SSBU stage pick helper

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ app = Flask(__name__)
 
 app.config['SERVER_NAME'] = 'kob.codes'
 
+real_url = 'ja.kob.codes'
+
 @app.route('/', subdomain='ja')
 def landing():
     return render_template('landingpage.html')
 
+@app.route('/stagepicker', subdomain='ja')
+def stagepicker():
+    return render_template('stagepicker.html')
+
 @app.route('/')
-def send_to_landing():
-    return redirect('http://ja.kob.codes')
+def root():
+    return redirect(real_url, code=302)
+
+
+@app.route('/<path:page>')
+def anypage(page):
+    return redirect('{real_url}/{page}'.format(page=page, real_url=real_url), code=302)
 
 if __name__ == '__main__':
     app.run()
